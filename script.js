02013c907d1093cb1276adfb7de5d08e28752615
gstaaij <gstaaij@protonmail.com>
2024-12-16 09:01:56 +0100

    Use a form and add two more strofes

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-const btnGenerate = document.querySelector("button#btnGenerate");
+const formControls = document.querySelector("form.controls");
+const btnGenerate = document.querySelector("input#btnGenerate");
 const inputStrofenAantal = document.querySelector("input#inputStrofenAantal");
 const gedichtContainer = document.querySelector(".gedichtContainer");
 const gedichtElem = gedichtContainer.querySelector(".gedicht");
 
-const btnGenerateText = btnGenerate.textContent;
-btnGenerate.textContent = "Aan het laden...";
+const btnGenerateText = btnGenerate.value;
+btnGenerate.value = "Aan het laden...";
 btnGenerate.disabled = true;
 let geladenDingen = 0;
 function woordenLaadCallback() {
     ++geladenDingen;
     if (geladenDingen >= 4) {
         btnGenerate.disabled = false;
-        btnGenerate.textContent = btnGenerateText;
+        btnGenerate.value = btnGenerateText;
     }
 }
 ggLaadStrofes()
@@ -24,6 +25,7 @@ ggLaadBijvoeglijkeNaamwoorden()
 ggLaadWerkwoorden()
     .then(woordenLaadCallback);
 
-btnGenerate.addEventListener("click", () => {
+formControls.addEventListener("submit", e => {
+    e.preventDefault();
     gedichtElem.textContent = ggGenereerGedicht(inputStrofenAantal.value);
 });
